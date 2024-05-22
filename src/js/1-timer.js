2179887e16563113ad3c038987cfae8b74b240c9
Oleksandr Volokhov <Flatroniym@gmail.com>
2024-05-22 16:58:51 +0200

    Made script for timer page

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,15 +1,3 @@
-//? Якщо користувач вибрав дату в минулому, покажи window.alert() з текстом "Please choose a date in the future" і зроби кнопку «Start» не активною.
-//? Якщо користувач вибрав валідну дату (в майбутньому), кнопка «Start» стає активною.
-//? Кнопка «Start» повинна бути неактивною доти, доки користувач не вибрав дату в майбутньому. Зверни увагу, що при обранні валідної дати, не запуску таймера і обранні потім невалідної дати, кнопка після розблокування має знову стати неактивною.
-// Натисканням на кнопку «Start» починається зворотний відлік часу до обраної дати з моменту натискання.
-// Натисканням на кнопку «Start» скрипт повинен обчислювати раз на секунду, скільки часу залишилось до вказаної дати, і оновлювати інтерфейс таймера, показуючи чотири цифри: дні, години, хвилини і секунди у форматі xx:xx:xx:xx.
-
-// Кількість днів може складатися з більше, ніж двох цифр.
-// Таймер повинен зупинятися, коли дійшов до кінцевої дати, тобто залишок часу дорівнює нулю 00:00:00:00.
-//! Після запуску таймера натисканням кнопки Старт кнопка Старт і інпут стають неактивним, щоб користувач не міг обрати нову дату, поки йде відлік часу. Після зупинки таймера інпут стає активним, щоб користувач міг обрати наступну дату. Кнопка залишається не активною.
-// Напиши функцію, наприклад addLeadingZero(value), яка використовує метод рядка padStart() і перед відмальовуванням інтерфейсу форматує значення.
-// Для відображення повідомлень користувачеві, замість window.alert(), використовуй бібліотеку iziToast.
-//Накинути стилі
 
 
 
@@ -17,28 +5,58 @@ import flatpickr from "flatpickr";
 import "flatpickr/dist/flatpickr.min.css";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
-const input = document.querySelector('input#datetime-picker');
-const button = document.querySelector('button[data-start]');
+const selectors = {
+	input: document.querySelector('input#datetime-picker'),
+	button: document.querySelector('button[data-start]'),
+	days: document.querySelector('span[data-days]'),
+	hours: document.querySelector('span[data-hours]'),
+	minutes: document.querySelector('span[data-minutes]'),
+	seconds: document.querySelector('span[data-seconds]'),
+};
+const { input, button, days, hours, minutes, seconds } = selectors;
+
 let userSelectedDate;
-const options = {
+let options = {
 	enableTime: true,
 	time_24hr: true,
 	defaultDate: new Date(),
 	minuteIncrement: 1,
+	allowInput: false,
 	onClose(selectedDates) {
-		const currentDate = new Date();
 		userSelectedDate = selectedDates[0];
-		const timeUntilEnd = userSelectedDate - currentDate;
-		console.log(timeUntilEnd);
+		const timeUntilEnd = userSelectedDate - new Date();
 		if (timeUntilEnd <= 0) {
-			alert("Please choose a date in the future");
+			iziToast.error({ title: "Error", message: "Please choose a date in the future" });
 			button.setAttribute('disabled', "");
 			return;
 		}
 		button.removeAttribute('disabled');
 	},
 };
+button.addEventListener('click', () => {
+	const intervalId = setInterval(() => {
+		const dateDifference = userSelectedDate - new Date();
+		const dateObj = convertMs(dateDifference);
+		days.innerHTML = addLeadingZero(dateObj.days);
+		hours.innerHTML = addLeadingZero(dateObj.hours);
+		minutes.innerHTML = addLeadingZero(dateObj.minutes);
+		seconds.innerHTML = addLeadingZero(dateObj.seconds);
+		button.setAttribute('disabled', "");
+		input.setAttribute('disabled', "");
+		if (dateDifference <= 0 || !dateDifference) {
+			clearInterval(intervalId);
+			days.innerHTML = "00";
+			hours.innerHTML = "00";
+			minutes.innerHTML = "00";
+			seconds.innerHTML = "00";
+			input.removeAttribute('disabled');
+		}
+	}, 1000);
 
+});
+function addLeadingZero(value) {
+	return value.toString().padStart(2, "0");
+}
 function convertMs(ms) {
 	// Number of milliseconds per unit of time
 	const second = 1000;
@@ -58,4 +76,5 @@ function convertMs(ms) {
 	return { days, hours, minutes, seconds };
 }
 
-flatpickr(input, options);
+flatpickr(input, options);;
+
